9bc79bf89de0e1b9594ccd398da7918a0aad85e7
Jordan Lawanson <jlawanson@college.harvard.edu>
2019-01-22 11:23:31 -0500

    frontend

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
 import Welcome from "../../components/Welcome";
 import Button from "../../components/Button";
+import SignUp from "../SignUp";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSignUpComponent: false
+      // showLogInComponent: false
+    };
+
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
   handleLogin = e => {
     e.preventDefault();
+    this.setState({
+      // showLogInComponent: true
+    });
+  };
+
+  handleSignUp = e => {
+    e.preventDefault();
+    // let path = `/signup`;
+    // this.props.history.push(path);
+    this.setState({
+      showSignUpComponent: true
+    });
   };
 
   render() {
     return (
       <React.Fragment>
         <Welcome />
-        <Button title="Login" onClick={e => this.handleLogin(e)}>
-          Login
-        </Button>
+        <div>
+          <Button title="Login" action={e => this.handleLogin(e)} />
+          {/* {this.state.showLogInComponent ? <Login /> : null} */}
+        </div>
+        or
+        <div>
+          <Button title="Sign Up" action={e => this.handleSignUp(e)} />
+          {this.state.showSignUpComponent ? <SignUp /> : null}
+        </div>
       </React.Fragment>
     );
   }

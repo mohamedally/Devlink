fc698ae3f7b3daa38f1244c07931c7c3312a58c1
Mohamed Ally Iddi <mohamed_iddi@college.harvard.edu>
2019-01-24 14:54:32 -0500

    Set up apollo client and apollo provider in the frontend

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import {
   Switch,
   Route,
   Redirect,
   BrowserRouter as Router
-} from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import theme from "./theme";
-import Home from "./containers/Home/";
-import Wall from "./containers/Wall/";
-import MyUserProfile from "./containers/MyUserProfile/";
-import Navbar from "./components/Navbar";
-import { StyledH3 } from "./styles";
-import User from "./containers/User";
-import Users from "./containers/Users";
+} from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import theme from "./theme"
+import Home from "./containers/Home/"
+import Wall from "./containers/Wall/"
+import MyUserProfile from "./containers/MyUserProfile/"
+import Navbar from "./components/Navbar"
+import { StyledH3 } from "./styles"
+import User from "./containers/User"
+import Users from "./containers/Users"
+import { ApolloProvider } from "react-apollo"
+import ApolloClient from "apollo-boost"
+import config from "./config"
+
+const client = new ApolloClient({
+  uri: config.graphqlUrl
+})
 
 class App extends Component {
   render() {
     return (
       <Router>
         <ThemeProvider theme={theme}>
-          <div className="App">
-            <StyledH3>devLink™</StyledH3>
-            <Navbar />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/wall" component={Wall} />
-              <Route path="/profile" component={MyUserProfile} />
-              <Route path="/user" component={User} />
-              <Route path="/users" component={Users} />
-              <Redirect to="/" />
-            </Switch>
-          </div>
+          <ApolloProvider client={client}>
+            <div className="App">
+              <StyledH3>devLink™</StyledH3>
+              <Navbar />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/wall" component={Wall} />
+                <Route path="/profile" component={MyUserProfile} />
+                <Route path="/user" component={User} />
+                <Route path="/users" component={Users} />
+                <Redirect to="/" />
+              </Switch>
+            </div>
+          </ApolloProvider>
         </ThemeProvider>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+  graphqlUrl: process.env.REACT_APP_API_URL || "htttp;//localhost:5000/graphql"
+}

568fd366efbcd39043092f13beeca4dfd077e9d5
Yooni Park <yooni_park@college.harvard.edu>
2019-01-24 15:48:02 -0500

    trial

--- src/containers/Users/index.js
+++ src/containers/Users/index.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Container } from "./styles";
 
 class Users extends React.Component {
   render() {
     return (
-      <ul>
-        <li>Jordan Lawanson</li>
-        <li>Yooni Park</li>
-        <li>Rob Walker</li>
-        <li>Mohammed Ally</li>
-      </ul>
+      <Container>
+        <ul
+            style={{
+              listStyle: "none",
+              margin: "0px",
+              padding: "0px",
+              textAlign: "center"
+          }}>
+          <li>Jordan Lawanson</li>
+          <li>Yooni Park</li>
+          <li>Rob Walker</li>
+          <li>Mohammed Ally</li>
+        </ul>
+      </Container>
     );
   }
 }
